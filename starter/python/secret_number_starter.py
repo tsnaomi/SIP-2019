3ae5126de63251dcae90d2476487f6f414d480b7
Naomi Shapiro <tsnaomi@uw.edu>
2019-07-21 09:37:04 -0700

    clean up starter code

--- starter/python/secret_number_starter.py
+++ starter/python/secret_number_starter.py
@@ -3,14 +3,14 @@
 import random
 
 # Generates a random integer.
-a_random_number = random.randint(1, 20)
+random_number = random.randint(1, 20)
 
 # DEBUG: To preview the random number, uncomment the following below:
-# print(a_random_number)
+# print(random_number)
 
 guess = input("Guess a number between 1 and 20 (inclusive): ")
 
-if not guess.isnumeric():  # Checks if the string consists only of digits.
+if not guess.isnumeric():  # Checks if the guess consists only of digits.
     print("That's not a positive whole number, try again!")
 
 else:
